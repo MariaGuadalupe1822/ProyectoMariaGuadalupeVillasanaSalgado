e1918504bc7465dffee41ca5b0513433c72b2dc9
Maria Guadalupe Villasana <22670010@iguala.tecnm.mx>
2023-03-02 13:26:35 -0600

    Se creo clase Student

--- src/Student.java
+++ src/Student.java
@@ -3,53 +3,73 @@ public class Student {
     private long controlNumber;
     private String name;
     private String lastname;
-    private String carrer;
-    private short semester;
+    private String career;
+    private int semester;
     private double average;
+
+    //atributos
+
+
+
+
+    //gets and sets son los metodos implicitos
+
+    public int getSemester() {
+        return this.semester;
+    }
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
 
 
     public double getGeneralAverage(long controlNumber) {
         return 0.0;
     }
-    public double getSemesterAverage(Long controlNumber, Short semester) {
-        return average ;
-    }
-    public double getsubjectAverage(Long controlNumber, String materia) {
+    public double getSemesterAverage(long controlNumber, int semester) {return 0.0 ;}
+    public double getSubjectGrade(long controlNumber, String Subject) {
         return 0.0;
     }
+
+    //metodos explicitos
+
+
+
+
     public long getControlNumber() {
-        return controlNumber;
+        return this.controlNumber;
     }
-
-
-    public void setControlNumber(Long controlNumber) {
+    public void setControlNumber(long controlNumber) {
         this.controlNumber = controlNumber;
     }
+
+
+
     public String getName() {
-        return name;
+        return this.name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
+
     public String getLastname() {
-        return lastname;
+        return this.lastname;
     }
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-    public String getCarrer() {
-        return carrer;
-    }
-    public void setCarrer(String carrer) {
-        this.carrer = carrer;
-    }
-    public Short getSemester() {
-        return semester;
-    }
 
-    public void setSemester(Short semester) {
-        this.semester = semester;
+
+
+    public String getCareer() {
+        return this.career;
     }
+    public void setCareer(String career) { this.career=career; }
+
+
+
+
 
     public double getAverage() {
         return average;
@@ -58,5 +78,9 @@ public class Student {
     public void setAverage(double average) {
         this.average = average;
     }
+
+
+
+
 }
-}
+
