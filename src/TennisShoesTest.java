7b90c83ea860a8165b83c20e686ee5414f191c7a
Maria Guadalupe Villasana <22670010@iguala.tecnm.mx>
2023-03-15 18:26:59 -0600

    "Se creo las clases TennisShoes"

--- src/TennisShoesTest.java
+++ src/TennisShoesTest.java
@@ -6,7 +6,6 @@ public class TennisShoesTest {
         TennisShoes tennisShoes = new TennisShoes("Maria", 5, 'F', "Nike", (byte) 25, "Negro", 22);
 
 
-tennisShoes.toString();
+        tennisShoes.toString();
     }
-    }
-
+}
