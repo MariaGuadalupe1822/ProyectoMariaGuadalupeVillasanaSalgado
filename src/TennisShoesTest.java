fb8e21f65f3d92cf13fe658207a396a5e47c320b
Maria Guadalupe Villasana <22670010@iguala.tecnm.mx>
2023-03-15 18:11:17 -0600

    "Se creo las clases patient"

---
+++ src/TennisShoesTest.java
@@ -0,0 +1,12 @@
+
+public class TennisShoesTest {
+
+    public static void main(String[] args) {
+
+        TennisShoes tennisShoes = new TennisShoes("Maria", 5, 'F', "Nike", (byte) 25, "Negro", 22);
+
+
+tennisShoes.toString();
+    }
+    }
+
